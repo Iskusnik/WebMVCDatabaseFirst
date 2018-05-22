c98800c89f02383866fc125cf5df1c52bb1a21e3
IskusnikXD <IskusnikXD@ISKUSNIK>
2018-05-22 11:26:37 +0500

    Добавьте файлы проекта.

---
+++ WebMVCDatabaseFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebMVCDatabaseFirst.MvcApplication" Language="C#" %>
---
+++ WebMVCDatabaseFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebMVCDatabaseFirst.Startup))]
+namespace WebMVCDatabaseFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
